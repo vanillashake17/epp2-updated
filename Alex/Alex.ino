b4b67a69dd7df1b0594f8179fe863262597988ac
shau <shauushee@gmail.com>
2026-03-20 18:05:07 +0800

    modify robotlib in Alex

--- Alex/Alex.ino
+++ Alex/Alex.ino
@@ -3,18 +3,23 @@ void setup() {}
 void loop() {
   // put your main code here, to run repeatedly:
 
-  forward(200); // takes values from 0 to 255
-  delay(2000);
-
-  ccw(200); // takes values from 0 to 255
-  delay(2000);
-
-  backward(200); // takes values from 0 to 255
-  delay(2000);
-
-  cw(200); // takes values from 0 to 255
-  delay(2000);
+  forward(255); // takes values from 0 to 255
+  delay(3000);
 
   stop();
-  delay(4000);
+  delay(2000);
+
+  ccw(255); // takes values from 0 to 255
+  delay(10000);
+
+
+  //backward(200); // takes values from 0 to 255
+  delay(10000);
+
+  stop();
+  delay(2000);
+
+  cw(255); 
+  delay(10000);
+  delay(1000000);
 }
