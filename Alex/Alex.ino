f253c28c5c5643a3d2012b7f99630f0b4ee0dcc8
shau <shauushee@gmail.com>
2026-03-22 22:17:35 +0800

    modify test settings

--- Alex/Alex.ino
+++ Alex/Alex.ino
@@ -3,23 +3,23 @@ void setup() {}
 void loop() {
   // put your main code here, to run repeatedly:
 
-  forward(255); // takes values from 0 to 255
+  forward(200); // takes values from 0 to 255
   delay(3000);
 
   stop();
   delay(2000);
 
-  ccw(255); // takes values from 0 to 255
-  delay(10000);
+  ccw(200); // takes values from 0 to 255
+  delay(3000);
 
 
-  //backward(200); // takes values from 0 to 255
-  delay(10000);
+  backward(200); // takes values from 0 to 255
+  delay(3000);
 
   stop();
   delay(2000);
 
-  cw(255); 
-  delay(10000);
+  cw(200); 
+  delay(3000);
   delay(1000000);
 }
