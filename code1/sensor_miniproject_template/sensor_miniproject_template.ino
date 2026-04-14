35374e569918c29bd9fa7ec202c1a8dea68b3fc7
shau <shauushee@gmail.com>
2026-04-14 23:27:16 +0800

    remove lidar optimisation changes, change home position

--- code1/sensor_miniproject_template/sensor_miniproject_template.ino
+++ code1/sensor_miniproject_template/sensor_miniproject_template.ino
@@ -55,14 +55,14 @@
 #define BASE_MAX 180  // base angle upper limit
 #define SHLD_MIN 110  // shoulder angle lower limit
 #define SHLD_MAX 180  // shoulder angle upper limit
-#define ELBW_MIN 0    // elbow angle lower limit
+#define ELBW_MIN 65    // elbow angle lower limit
 #define ELBW_MAX 180   // elbow angle upper limit
 #define GRIP_MIN 5    // gripper angle lower limit
 #define GRIP_MAX 33   // gripper angle upper limit
 
 #define BASE_HOME 80  // base home angle
-#define SHLD_HOME 140  // shoulder home angle
-#define ELBW_HOME 90   // elbow home angle
+#define SHLD_HOME 110  // shoulder home angle
+#define ELBW_HOME 60   // elbow home angle
 #define GRIP_HOME 25   // gripper home angle
 
 #define BASE_CHECKPOINT 0      // staggered checkpoint (timer ticks)
